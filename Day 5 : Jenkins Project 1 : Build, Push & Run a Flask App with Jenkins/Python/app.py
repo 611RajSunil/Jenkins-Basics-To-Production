555b36e2bdb4df5850ecd450bcea16d7b2908dad
611RajSunil <59165588+611RajSunil@users.noreply.github.com>
2026-09-23 17:03:49 +0530

    Update app.py

--- Day 5 : Jenkins Project 1 : Build, Push & Run a Flask App with Jenkins/Python/app.py
+++ Day 5 : Jenkins Project 1 : Build, Push & Run a Flask App with Jenkins/Python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.get("/")
 def hello():
     return jsonify(
-        message="✨ Welcome to  ✨",
+        message="✨ Welcome to My FLASK App ✨",
         tip="Built with Flask, shipped by Jenkins, running in Docker."
     )
 
